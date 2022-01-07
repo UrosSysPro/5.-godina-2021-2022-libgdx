942bdb0377f00f05b2815c4cf56ced3351fac653
Uros <karaleicuu@gmail.com>
2022-01-07 17:48:18 +0100

    ae vise

---
+++ projekat/core/src/com/systempro/uros/projekat/shaderPaint/Main.java
@@ -0,0 +1,15 @@
+package com.systempro.uros.projekat.shaderPaint;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Main extends ApplicationAdapter {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
--- projekat/desktop/src/com/systempro/uros/projekat/desktop/DesktopLauncher.java
+++ projekat/desktop/src/com/systempro/uros/projekat/desktop/DesktopLauncher.java
@@ -3,8 +3,8 @@ package com.systempro.uros.projekat.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.systempro.uros.projekat.Rose;
-import com.systempro.uros.projekat.shader2.Main;
 import com.systempro.uros.projekat.shader2.Main2;
+import com.systempro.uros.projekat.shaderPaint.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
